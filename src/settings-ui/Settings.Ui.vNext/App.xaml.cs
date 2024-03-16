d0730be1334d19631303cd3dcf7954e0b9d64677
Aaron Junker <aaron.junker@outlook.com>
2024-03-16 21:43:57 +0100

    [WIP]

--- src/settings-ui/Settings.Ui.vNext/App.xaml.cs
+++ src/settings-ui/Settings.Ui.vNext/App.xaml.cs
@@ -1,8 +1,26 @@
-﻿// Copyright (c) Microsoft Corporation
+// Copyright (c) Microsoft Corporation
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.CodeDom;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.UI;
+using interop;
+using ManagedCommon;
+using Microsoft.PowerToys.Settings.UI.Library;
+using Microsoft.PowerToys.Settings.UI.Library.Telemetry.Events;
+using Microsoft.PowerToys.Telemetry;
 using Microsoft.UI.Xaml;
+using Settings.Ui.VNext;
+using Settings.Ui.VNext.Helpers;
+using Settings.Ui.VNext.Views;
+using Windows.UI.Popups;
+using WinRT.Interop;
+using WinUIEx;
 
 namespace Settings.Ui.VNext
 {
@@ -11,17 +29,344 @@ namespace Settings.Ui.VNext
     /// </summary>
     public partial class App : Application
     {
+        private enum Arguments
+        {
+            PTPipeName = 1,
+            SettingsPipeName,
+            PTPid,
+            Theme, // used in the old settings
+            ElevatedStatus,
+            IsUserAdmin,
+            ShowOobeWindow,
+            ShowScoobeWindow,
+            ShowFlyout,
+            ContainsSettingsWindow,
+            ContainsFlyoutPosition,
+        }
+
+        // Quantity of arguments
+        private const int RequiredArgumentsQty = 12;
+
+        // Create an instance of the  IPC wrapper.
+        private static TwoWayPipeMessageIPCManaged ipcmanager;
+
+        public static bool IsElevated { get; set; }
+
+        public static bool IsUserAnAdmin { get; set; }
+
+        public static int PowerToysPID { get; set; }
+
+        public bool ShowOobe { get; set; }
+
+        public bool ShowFlyout { get; set; }
+
+        public bool ShowScoobe { get; set; }
+
+        public Type StartupPage
+        {
+            get { throw new NotImplementedException(); }
+            set { }
+        }
+
+        public static Action<string> IPCMessageReceivedCallback { get; set; }
+
+        private static bool loggedImmersiveDarkException;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// Initializes the singleton application object. This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
         public App()
         {
-            InitializeComponent();
+            Logger.InitializeLogger("\\Settings\\Logs");
+
+            this.InitializeComponent();
         }
 
+        public static void OpenSettingsWindow(Type type = null, bool ensurePageIsSelected = false)
+        {
+            if (settingsWindow == null)
+            {
+                settingsWindow = new MainWindow();
+            }
+
+            settingsWindow.Activate();
+
+            if (type != null)
+            {
+                settingsWindow.NavigateToSection(type);
+
+                WindowHelpers.BringToForeground(settingsWindow.GetWindowHandle());
+            }
+
+            if (ensurePageIsSelected)
+            {
+                settingsWindow.EnsurePageIsSelected();
+            }
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            _window = new MainWindow();
-            _window.Activate();
+            var cmdArgs = Environment.GetCommandLineArgs();
+
+            if (cmdArgs != null && cmdArgs.Length >= RequiredArgumentsQty)
+            {
+                // Skip the first argument which is prepended when launched by explorer
+                if (cmdArgs[0].EndsWith(".dll", StringComparison.InvariantCultureIgnoreCase) && cmdArgs[1].EndsWith(".exe", StringComparison.InvariantCultureIgnoreCase) && (cmdArgs.Length >= RequiredArgumentsQty + 1))
+                {
+                    cmdArgs = cmdArgs.Skip(1).ToArray();
+                }
+
+                _ = int.TryParse(cmdArgs[(int)Arguments.PTPid], out int powerToysPID);
+                PowerToysPID = powerToysPID;
+
+                IsElevated = cmdArgs[(int)Arguments.ElevatedStatus] == "true";
+                IsUserAnAdmin = cmdArgs[(int)Arguments.IsUserAdmin] == "true";
+                ShowOobe = cmdArgs[(int)Arguments.ShowOobeWindow] == "true";
+                ShowScoobe = cmdArgs[(int)Arguments.ShowScoobeWindow] == "true";
+                ShowFlyout = cmdArgs[(int)Arguments.ShowFlyout] == "true";
+                bool containsSettingsWindow = cmdArgs[(int)Arguments.ContainsSettingsWindow] == "true";
+                bool containsFlyoutPosition = cmdArgs[(int)Arguments.ContainsFlyoutPosition] == "true";
+
+                // To keep track of variable arguments
+                int currentArgumentIndex = RequiredArgumentsQty;
+
+                if (containsSettingsWindow)
+                {
+                    // Open specific window
+                    StartupPage = GetPage(cmdArgs[currentArgumentIndex]);
+
+                    currentArgumentIndex++;
+                }
+
+                int flyout_x = 0;
+                int flyout_y = 0;
+                if (containsFlyoutPosition)
+                {
+                    // get the flyout position arguments
+                    _ = int.TryParse(cmdArgs[currentArgumentIndex++], out flyout_x);
+                    _ = int.TryParse(cmdArgs[currentArgumentIndex++], out flyout_y);
+                }
+
+                RunnerHelper.WaitForPowerToysRunner(PowerToysPID, () =>
+                {
+                    Environment.Exit(0);
+                });
+
+                ipcmanager = new TwoWayPipeMessageIPCManaged(cmdArgs[(int)Arguments.SettingsPipeName], cmdArgs[(int)Arguments.PTPipeName], (string message) =>
+                {
+                    if (IPCMessageReceivedCallback != null && message.Length > 0)
+                    {
+                        IPCMessageReceivedCallback(message);
+                    }
+                });
+                ipcmanager.Start();
+
+                /*
+                if (!ShowOobe && !ShowScoobe && !ShowFlyout)
+                {
+                    settingsWindow = new MainWindow();
+                    settingsWindow.Activate();
+                    settingsWindow.ExtendsContentIntoTitleBar = true;
+                    settingsWindow.NavigateToSection(StartupPage);
+
+                    // https://github.com/microsoft/microsoft-ui-xaml/issues/7595 - Activate doesn't bring window to the foreground
+                    // Need to call SetForegroundWindow to actually gain focus.
+                    WindowHelpers.BringToForeground(settingsWindow.GetWindowHandle());
+                }
+                else
+                {
+                    // Create the Settings window hidden so that it's fully initialized and
+                    // it will be ready to receive the notification if the user opens
+                    // the Settings from the tray icon.
+                    settingsWindow = new MainWindow(true);
+
+                    if (ShowOobe)
+                    {
+                        PowerToysTelemetry.Log.WriteEvent(new OobeStartedEvent());
+                        OobeWindow oobeWindow = new OobeWindow(OOBE.Enums.PowerToysModules.Overview);
+                        oobeWindow.Activate();
+                        oobeWindow.ExtendsContentIntoTitleBar = true;
+                    }
+                    else if (ShowScoobe)
+                    {
+                        PowerToysTelemetry.Log.WriteEvent(new ScoobeStartedEvent());
+                        OobeWindow scoobeWindow = new OobeWindow(OOBE.Enums.PowerToysModules.WhatsNew);
+                        scoobeWindow.Activate();
+                        scoobeWindow.ExtendsContentIntoTitleBar = true;
+                    }
+                    else if (ShowFlyout)
+                    {
+                        POINT? p = null;
+                        if (containsFlyoutPosition)
+                        {
+                            p = new POINT(flyout_x, flyout_y);
+                        }
+
+                        ShellPage.OpenFlyoutCallback(p);
+                    }
+                }
+*/
+                if (SelectedTheme() == ElementTheme.Default)
+                {
+                    try
+                    {
+                        themeListener = new ThemeListener();
+                        themeListener.ThemeChanged += (_) => HandleThemeChange();
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.LogError($"HandleThemeChange exception. Please install .NET 4.", ex);
+                    }
+                }
+            }
+            else
+            {
+#if DEBUG
+                // For debugging purposes
+                // Window is also needed to show MessageDialog
+                settingsWindow = new MainWindow();
+                settingsWindow.ExtendsContentIntoTitleBar = true;
+                settingsWindow.Activate();
+
+                // settingsWindow.NavigateToSection(StartupPage);
+                ShowMessageDialog("The application is running in Debug mode.", "DEBUG");
+#else
+                /* If we try to run Settings as a standalone app, it will start PowerToys.exe if not running and open Settings again through it in the Dashboard page. */
+                SettingsDeepLink.OpenSettings(SettingsDeepLink.SettingsWindow.Dashboard, true);
+                Exit();
+#endif
+            }
         }
 
-        private Window _window;
+#if !DEBUG
+        private async void ShowMessageDialogAndExit(string content, string title = null)
+#else
+        private async void ShowMessageDialog(string content, string title = null)
+#endif
+        {
+            await ShowDialogAsync(content, title);
+#if !DEBUG
+            this.Exit();
+#endif
+        }
+
+        public static Task<IUICommand> ShowDialogAsync(string content, string title = null)
+        {
+            var dialog = new MessageDialog(content, title ?? string.Empty);
+            var handle = NativeMethods.GetActiveWindow();
+            if (handle == IntPtr.Zero)
+            {
+                throw new InvalidOperationException();
+            }
+
+            InitializeWithWindow.Initialize(dialog, handle);
+            return dialog.ShowAsync().AsTask<IUICommand>();
+        }
+
+        public static TwoWayPipeMessageIPCManaged GetTwoWayIPCManager()
+        {
+            return ipcmanager;
+        }
+
+        public static ElementTheme SelectedTheme()
+        {
+            switch (SettingsRepository<GeneralSettings>.GetInstance(settingsUtils).SettingsConfig.Theme.ToUpper(CultureInfo.InvariantCulture))
+            {
+                case "DARK": return ElementTheme.Dark;
+                case "LIGHT": return ElementTheme.Light;
+                default: return ElementTheme.Default;
+            }
+        }
+
+        public static bool IsDarkTheme()
+        {
+            var selectedTheme = SelectedTheme();
+            return selectedTheme == ElementTheme.Dark || (selectedTheme == ElementTheme.Default && ThemeHelpers.GetAppTheme() == AppTheme.Dark);
+        }
+
+        public static void HandleThemeChange()
+        {
+            try
+            {
+                bool isDark = IsDarkTheme();
+
+                if (settingsWindow != null)
+                {
+                    var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(settingsWindow);
+                    ThemeHelpers.SetImmersiveDarkMode(hWnd, isDark);
+                }
+
+                if (SelectedTheme() == ElementTheme.Default)
+                {
+                    themeListener = new ThemeListener();
+                    themeListener.ThemeChanged += (_) => HandleThemeChange();
+                }
+                else if (themeListener != null)
+                {
+                    themeListener.Dispose();
+                    themeListener = null;
+                }
+            }
+            catch (Exception e)
+            {
+                if (!loggedImmersiveDarkException)
+                {
+                    Logger.LogError($"HandleThemeChange exception. Please install .NET 4.", e);
+                    loggedImmersiveDarkException = true;
+                }
+            }
+        }
+
+        public static int UpdateUIThemeMethod(string themeName)
+        {
+            switch (themeName?.ToUpperInvariant())
+            {
+                case "LIGHT":
+                    // OobeShellPage.OobeShellHandler.RequestedTheme = ElementTheme.Light;
+                    ShellPage.ShellHandler.RequestedTheme = ElementTheme.Light;
+                    break;
+                case "DARK":
+                    // OobeShellPage.OobeShellHandler.RequestedTheme = ElementTheme.Dark;
+                    ShellPage.ShellHandler.RequestedTheme = ElementTheme.Dark;
+                    break;
+                case "SYSTEM":
+                    // OobeShellPage.OobeShellHandler.RequestedTheme = ElementTheme.Default;
+                    ShellPage.ShellHandler.RequestedTheme = ElementTheme.Default;
+                    break;
+                default:
+                    Logger.LogError($"Unexpected theme name: {themeName}");
+                    break;
+            }
+
+            HandleThemeChange();
+            return 0;
+        }
+
+        private static ISettingsUtils settingsUtils = new SettingsUtils();
+
+        private static MainWindow settingsWindow;
+        private static ThemeListener themeListener;
+
+        public static void ClearSettingsWindow()
+        {
+            settingsWindow = null;
+        }
+
+        public static MainWindow GetSettingsWindow()
+        {
+            return settingsWindow;
+        }
+
+        public static Type GetPage(string settingWindow)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/settings-ui/Settings.Ui.vNext/MainWindow.xaml.cs
+++ src/settings-ui/Settings.Ui.vNext/MainWindow.xaml.cs
@@ -1,24 +1,183 @@
-// Copyright (c) Microsoft Corporation
+﻿// Copyright (c) Microsoft Corporation
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using ManagedCommon;
+using Microsoft.PowerLauncher.Telemetry;
+using Microsoft.PowerToys.Settings.UI.Library;
+using Microsoft.PowerToys.Telemetry;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using Settings.Ui.VNext.Helpers;
+using Settings.Ui.VNext.Views;
+using Windows.Data.Json;
+using WinUIEx;
 
 namespace Settings.Ui.VNext
 {
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class MainWindow : WindowEx
     {
-        public MainWindow()
+        public MainWindow(bool createHidden = false)
         {
-            InitializeComponent();
+            var bootTime = new System.Diagnostics.Stopwatch();
+            bootTime.Start();
+
+            ShellPage.SetElevationStatus(App.IsElevated);
+            ShellPage.SetIsUserAnAdmin(App.IsUserAnAdmin);
+
+            // Set window icon
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+            appWindow.SetIcon("Assets\\Settings\\icon.ico");
+
+            var placement = Utils.DeserializePlacementOrDefault(hWnd);
+            if (createHidden)
+            {
+                placement.ShowCmd = NativeMethods.SW_HIDE;
+
+                // Restore the last known placement on the first activation
+                this.Activated += Window_Activated;
+            }
+
+            NativeMethods.SetWindowPlacement(hWnd, ref placement);
+
+            var loader = ResourceLoaderInstance.ResourceLoader;
+            Title = App.IsElevated ? loader.GetString("SettingsWindow_AdminTitle") : loader.GetString("SettingsWindow_Title");
+
+            // send IPC Message
+            ShellPage.SetDefaultSndMessageCallback(msg =>
+            {
+                // IPC Manager is null when launching runner directly
+                App.GetTwoWayIPCManager()?.Send(msg);
+            });
+
+            // send IPC Message
+            ShellPage.SetRestartAdminSndMessageCallback(msg =>
+            {
+                App.GetTwoWayIPCManager()?.Send(msg);
+                Environment.Exit(0); // close application
+            });
+
+            // send IPC Message
+            ShellPage.SetCheckForUpdatesMessageCallback(msg =>
+            {
+                App.GetTwoWayIPCManager()?.Send(msg);
+            });
+
+            // open main window
+            ShellPage.SetOpenMainWindowCallback(type =>
+            {
+                DispatcherQueue.TryEnqueue(Microsoft.UI.Dispatching.DispatcherQueuePriority.Normal, () =>
+                     App.OpenSettingsWindow(type));
+            });
+
+            // open main window
+            ShellPage.SetUpdatingGeneralSettingsCallback((ModuleType moduleType, bool isEnabled) =>
+            {
+                SettingsRepository<GeneralSettings> repository = SettingsRepository<GeneralSettings>.GetInstance(new SettingsUtils());
+                GeneralSettings generalSettingsConfig = repository.SettingsConfig;
+                bool needToUpdate = ModuleHelper.GetIsModuleEnabled(generalSettingsConfig, moduleType) != isEnabled;
+
+                if (needToUpdate)
+                {
+                    ModuleHelper.SetIsModuleEnabled(generalSettingsConfig, moduleType, isEnabled);
+                    var outgoing = new OutGoingGeneralSettings(generalSettingsConfig);
+                    this.DispatcherQueue.TryEnqueue(Microsoft.UI.Dispatching.DispatcherQueuePriority.Normal, () =>
+                    {
+                        ShellPage.SendDefaultIPCMessage(outgoing.ToString());
+                        ShellPage.ShellHandler?.SignalGeneralDataUpdate();
+                    });
+                }
+
+                return needToUpdate;
+            });
+
+            // open oobe
+            ShellPage.SetOpenOobeCallback(() =>
+            {
+                throw new NotImplementedException();
+            });
+
+            // open whats new window
+            ShellPage.SetOpenWhatIsNewCallback(() =>
+            {
+                throw new NotImplementedException();
+            });
+
+            // open flyout
+            ShellPage.SetOpenFlyoutCallback((POINT? p) =>
+            {
+                throw new NotImplementedException();
+            });
+            this.InitializeComponent();
+
+            // receive IPC Message
+            App.IPCMessageReceivedCallback = (string msg) =>
+            {
+                if (ShellPage.ShellHandler.IPCResponseHandleList != null)
+                {
+                    var success = JsonObject.TryParse(msg, out JsonObject json);
+                    if (success)
+                    {
+                        foreach (Action<JsonObject> handle in ShellPage.ShellHandler.IPCResponseHandleList)
+                        {
+                            handle(json);
+                        }
+                    }
+                    else
+                    {
+                        Logger.LogError("Failed to parse JSON from IPC message.");
+                    }
+                }
+            };
+
+            bootTime.Stop();
+
+            PowerToysTelemetry.Log.WriteEvent(new SettingsBootEvent() { BootTimeMs = bootTime.ElapsedMilliseconds });
         }
 
-        private void MyButton_Click(object sender, RoutedEventArgs e)
+        public void NavigateToSection(System.Type type)
         {
-            myButton.Content = "Clicked";
+            ShellPage.Navigate(type);
+        }
+
+        public void CloseHiddenWindow()
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            if (!NativeMethods.IsWindowVisible(hWnd))
+            {
+                Close();
+            }
+        }
+
+        private void Window_Closed(object sender, WindowEventArgs args)
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            Utils.SerializePlacement(hWnd);
+
+            // todo
+        }
+
+        private void Window_Activated(object sender, WindowActivatedEventArgs args)
+        {
+            if (args.WindowActivationState != WindowActivationState.Deactivated)
+            {
+                this.Activated -= Window_Activated;
+                var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+                var placement = Utils.DeserializePlacementOrDefault(hWnd);
+                NativeMethods.SetWindowPlacement(hWnd, ref placement);
+            }
+        }
+
+        internal void EnsurePageIsSelected()
+        {
+            ShellPage.EnsurePageIsSelected();
         }
     }
 }
