f7556010a08d38e75951f42b703b78b8ab8dccaa
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2024-09-20 12:19:09 -0700

    Add MicrosoftLearnDocSearch Extension Prototype

---
+++ src/modules/cmdpal/Exts/MicrosoftLearnExtension/LinkAction.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using Microsoft.CmdPal.Extensions.Helpers;
+
+namespace MicrosoftLearnExtension;
+
+internal sealed partial class LinkAction : InvokableCommand
+{
+    private readonly SearchResult _post;
+
+    internal LinkAction(SearchResult post)
+    {
+        this._post = post;
+        this.Name = "Open link";
+        this.Icon = new("\uE8A7");
+    }
+
+    public override CommandResult Invoke()
+    {
+        Process.Start(new ProcessStartInfo(_post.Url) { UseShellExecute = true });
+        return CommandResult.KeepOpen();
+    }
+}
