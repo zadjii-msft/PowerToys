db128e8f20b6de559c9b6deda7fe26b76916afd8
Seraphima <zykovas91@gmail.com>
2024-10-16 11:47:56 +0200

    added indexer project

---
+++ src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.Indexer/IndexerCommandsProvider.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using Microsoft.CmdPal.Extensions;
+using Microsoft.CmdPal.Extensions.Helpers;
+
+namespace Microsoft.CmdPal.Ext.Indexer;
+
+public partial class IndexerActionsProvider : ICommandProvider
+{
+    public string DisplayName => $"Indexer Commands";
+
+    public IconDataType Icon => new(string.Empty);
+
+    private readonly IListItem[] _commands = [
+        new ListItem(new IndexerPage()),
+    ];
+
+#pragma warning disable CA1816 // Dispose methods should call SuppressFinalize
+    public void Dispose() => throw new NotImplementedException();
+#pragma warning restore CA1816 // Dispose methods should call SuppressFinalize
+
+    public IListItem[] TopLevelCommands()
+    {
+        return _commands;
+    }
+}
