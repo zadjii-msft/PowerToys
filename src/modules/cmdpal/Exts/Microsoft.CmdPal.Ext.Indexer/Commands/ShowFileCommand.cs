5d8726b0f520a83990b5343b3ec5ef0d4de0b3ff
Seraphima <zykovas91@gmail.com>
2024-10-16 17:08:50 +0200

    added command

---
+++ src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.Indexer/Commands/ShowFileCommand.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using System.IO;
+using Microsoft.CmdPal.Ext.Indexer.Data;
+using Microsoft.CmdPal.Extensions.Helpers;
+
+namespace Microsoft.CmdPal.Ext.Indexer.Commands;
+
+internal sealed partial class ShowFileCommand : InvokableCommand
+{
+    private readonly IndexerItem _item;
+
+    internal ShowFileCommand(IndexerItem item)
+    {
+        this._item = item;
+        this.Name = "Show in folder"; // TODO: localize
+        this.Icon = new("\uE838");
+    }
+
+    public override CommandResult Invoke()
+    {
+        if (File.Exists(_item.FullPath))
+        {
+            try
+            {
+                var argument = "/select, \"" + _item.FullPath + "\"";
+                Process.Start("explorer.exe", argument);
+            }
+            catch
+            {
+                // TODO: log
+            }
+        }
+
+        return CommandResult.KeepOpen();
+    }
+}
