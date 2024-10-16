1c2608b0c3dc647f5eedf68fefa6bfb8084c95ac
Seraphima <zykovas91@gmail.com>
2024-10-16 16:09:48 +0200

    add IndexerItem, show sample data

--- src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.Indexer/Pages/IndexerPage.cs
+++ src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.Indexer/Pages/IndexerPage.cs
@@ -2,6 +2,11 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CmdPal.Ext.Indexer.Data;
 using Microsoft.CmdPal.Extensions;
 using Microsoft.CmdPal.Extensions.Helpers;
 
@@ -17,13 +22,43 @@ internal sealed partial class IndexerPage : ListPage
 
     public override ISection[] GetItems()
     {
-        return [
-            new ListSection()
+        return DoGetItems();
+    }
+
+    private static /*async Task<List<IndexerItem>>*/ List<IndexerItem> GetFiles()
+    {
+        var items = new List<IndexerItem>();
+
+        var currentDir = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        var files = Directory.GetFiles(currentDir);
+
+        foreach (var file in files)
+        {
+            var fileName = Path.GetFileName(file);
+
+            items.Add(new IndexerItem()
             {
-                Items = [
-                    new ListItem(new NoOpCommand()) { Title = "TODO: Implement your extension here" }
-                ],
-            }
-        ];
+                FullPath = file,
+                FileName = fileName,
+            });
+        }
+
+        return items;
+    }
+
+    private /*async Task<ISection[]>*/ ISection[] DoGetItems()
+    {
+        List<IndexerItem> items = /*await*/ GetFiles();
+        var s = new ListSection()
+        {
+            Title = "Files", // TODO: localize
+            Items = items.Select((item) => new ListItem(new NoOpCommand())
+            {
+                Title = item.FileName,
+                Subtitle = item.FullPath,
+            }).ToArray(),
+        };
+
+        return [s];
     }
 }
