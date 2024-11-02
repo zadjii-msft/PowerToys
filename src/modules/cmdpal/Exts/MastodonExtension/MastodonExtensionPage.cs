93b7515c2f660368520e6fa0f122c5a78b51a964
Mike Griese <migrie@microsoft.com>
2024-11-02 09:08:20 -0500

    Fetch posts and deserialize them

--- src/modules/cmdpal/Exts/MastodonExtension/MastodonExtensionPage.cs
+++ src/modules/cmdpal/Exts/MastodonExtension/MastodonExtensionPage.cs
@@ -3,6 +3,12 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 using Microsoft.CmdPal.Extensions;
 using Microsoft.CmdPal.Extensions.Helpers;
 
@@ -11,6 +17,9 @@ namespace MastodonExtension;
 [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "This is sample code")]
 internal sealed partial class MastodonExtensionPage : ListPage
 {
+    private static readonly HttpClient Client = new();
+    private static readonly JsonSerializerOptions Options = new() { PropertyNameCaseInsensitive = true };
+
     public MastodonExtensionPage()
     {
         Icon = new("https://mastodon.social/packs/media/icons/android-chrome-36x36-4c61fdb42936428af85afdbf8c6a45a8.png");
@@ -19,9 +28,38 @@ internal sealed partial class MastodonExtensionPage : ListPage
 
     public override IListItem[] GetItems()
     {
-        return [
-            new ListItem(new NoOpCommand()) { Title = "TODO: Implement your extension here" }
-        ];
+        var postsAsync = FetchExplorePage();
+        postsAsync.ConfigureAwait(false);
+        var posts = postsAsync.Result;
+        return posts
+            .Select(p => new ListItem(new NoOpCommand())
+            {
+                Title = p.Content,
+                Subtitle = p.Account.Username,
+            })
+            .ToArray();
+    }
+
+    public async Task<List<MastodonStatus>> FetchExplorePage()
+    {
+        var statuses = new List<MastodonStatus>();
+
+        try
+        {
+            // Make a GET request to the Mastodon trends API endpoint
+            HttpResponseMessage response = await Client.GetAsync("https://mastodon.social/api/v1/trends/statuses");
+            response.EnsureSuccessStatusCode();
+
+            // Read and deserialize the response JSON into a list of MastodonStatus objects
+            var responseBody = await response.Content.ReadAsStringAsync();
+            statuses = JsonSerializer.Deserialize<List<MastodonStatus>>(responseBody, Options);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"An error occurred: {e.Message}");
+        }
+
+        return statuses;
     }
 }
 
@@ -44,4 +82,33 @@ public partial class MastodonExtensionActionsProvider : ICommandProvider
     {
         return _actions;
     }
+}
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "This is sample code")]
+public class MastodonStatus
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("content")]
+    public string Content { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("account")]
+    public MastodonAccount Account { get; set; }
+}
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "This is sample code")]
+public class MastodonAccount
+{
+    [JsonPropertyName("username")]
+    public string Username { get; set; }
+
+    [JsonPropertyName("display_name")]
+    public string DisplayName { get; set; }
+
+    [JsonPropertyName("avatar")]
+    public string Avatar { get; set; }
 }
