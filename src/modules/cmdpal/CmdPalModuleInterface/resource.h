1a782b662f9f72f49fe8a0b816a630f102df6ce7
Stefan Markovic <stefan@janeasystems.com>
2024-11-16 22:05:57 +0100

    version module iface dll

---
+++ src/modules/cmdpal/CmdPalModuleInterface/resource.h
@@ -0,0 +1,13 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by AlwaysOnTopModuleInterface.rc
+
+//////////////////////////////
+// Non-localizable
+
+#define FILE_DESCRIPTION "PowerToys Comand Palette Module"
+#define INTERNAL_NAME "PowerToys.CmdPalModuleInterface"
+#define ORIGINAL_FILENAME "PowerToys.CmdPalModuleInterface.dll"
+
+// Non-localizable
+//////////////////////////////
