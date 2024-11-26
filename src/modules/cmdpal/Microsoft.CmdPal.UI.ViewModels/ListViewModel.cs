3f5728d32bc52e1c05da7f6224ef46e502695714
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2024-11-26 15:11:44 -0800

    Add Loading State to ListPage and have it initiate the background thread for loading the ViewModel

--- src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/ListViewModel.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/ListViewModel.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Reflection;
 using CommunityToolkit.Mvvm.Collections;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -11,27 +12,41 @@ using Microsoft.CmdPal.UI.ViewModels.Messages;
 
 namespace Microsoft.CmdPal.UI.ViewModels;
 
-public partial class ListViewModel : ObservableObject
+public partial class ListViewModel(IListPage _model) : ObservableObject
 {
     // Observable from MVVM Toolkit will auto create public properties that use INotifyPropertyChange change
     // https://learn.microsoft.com/dotnet/communitytoolkit/mvvm/observablegroupedcollections for grouping support
     [ObservableProperty]
     public partial ObservableGroupedCollection<string, ListItemViewModel> Items { get; set; } = [];
 
-    public ListViewModel(IListPage model)
+    [ObservableProperty]
+    public partial bool IsInitialized { get; private set; }
+
+    //// Run on background thread from ListPage.xaml.cs
+    [RelayCommand]
+    private async Task<bool> InitializeAsync()
     {
+        // TODO: We may want a SemaphoreSlim lock here.
+
+        // TODO: We may want to investigate using some sort of AsyncEnumerable or populating these as they come in to the UI layer
+        //       Though we have to think about threading here and circling back to the UI thread with a TaskScheduler.
+
         // TEMPORARY: just plop all the items into a single group
         // see 9806fe5d8 for the last commit that had this with sections
         ObservableGroup<string, ListItemViewModel> group = new(string.Empty);
 
-        foreach (var item in model.GetItems())
+        foreach (var item in _model.GetItems())
         {
             ListItemViewModel viewModel = new(item);
-            _ = viewModel.InitializePropertiesAsync();
+            await viewModel.InitializePropertiesAsync();
             group.Add(viewModel);
         }
 
         Items.AddGroup(group);
+
+        IsInitialized = true;
+
+        return true;
     }
 
     // InvokeItemCommand is what this will be in Xaml due to source generator
