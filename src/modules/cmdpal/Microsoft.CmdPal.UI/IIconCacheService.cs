b0619db5b0c2d2d8eddab5baab8b891a65a405c7
Mike Griese <zadjii@gmail.com>
2024-12-06 13:36:35 -0600

    the stupidest possible way of doing this

---
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/IIconCacheService.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CmdPal.Extensions;
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace Microsoft.CmdPal.UI;
+
+public interface IIconCacheService
+{
+    public Task<IconSource?> GetIconSource(IconDataType icon);
+}
