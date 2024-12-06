f0f2076b1f74be02271630dcf3f52a0232189595
Mike Griese <zadjii@gmail.com>
2024-12-06 17:14:20 -0600

    this is the most XAML thing I've ever done

---
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/Controls/LoadIconBehavior.xaml.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CmdPal.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
+
+namespace Microsoft.CmdPal.UI.Controls;
+
+public partial class LoadIconBehavior : DependencyObject, IBehavior
+{
+    public IconDataType Source
+    {
+        get => (IconDataType)GetValue(SourceProperty);
+        set
+        {
+            SetValue(SourceProperty, value);
+            OnSourcePropertyChanged();
+        }
+    }
+
+    // Using a DependencyProperty as the backing store for Source.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty SourceProperty =
+        DependencyProperty.Register("Source", typeof(IconDataType), typeof(LoadIconBehavior), new PropertyMetadata(new IconDataType(string.Empty)));
+
+    public DependencyObject? AssociatedObject { get; private set; }
+
+    public void Attach(DependencyObject associatedObject) => AssociatedObject = associatedObject;
+
+    public void Detach() => AssociatedObject = null;
+
+    public async void OnSourcePropertyChanged()
+    {
+        // TODO: Cache this in the class
+        var iconService = App.Current.Services.GetService<IIconCacheService>()!;
+        var icoSource = await iconService.GetIconSource(Source ?? new(string.Empty));
+
+        if (AssociatedObject is Border border)
+        {
+            border.Child = icoSource?.CreateIconElement();
+        }
+    }
+}
