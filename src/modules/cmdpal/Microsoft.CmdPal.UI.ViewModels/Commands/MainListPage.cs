9bf4d2e77bd4181d52ba1b80ba5c13b2f7cffba5
Mike Griese <zadjii@gmail.com>
2024-12-13 12:31:58 -0600

    show a loading bar while we're loading up

--- src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/Commands/MainListPage.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/Commands/MainListPage.cs
@@ -4,6 +4,7 @@
 
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Diagnostics;
 using Microsoft.CmdPal.Ext.Apps.Programs;
 using Microsoft.CmdPal.Extensions;
 using Microsoft.CmdPal.Extensions.Helpers;
@@ -23,23 +24,46 @@ public partial class MainListPage : DynamicListPage
 
     private IEnumerable<IListItem>? _filteredItems;
 
+    private bool _appsLoading = true;
+
     public MainListPage(IServiceProvider serviceProvider)
     {
         Name = "Command Palette";
+
         _serviceProvider = serviceProvider;
 
-        var tlcManager = _serviceProvider.GetService<TopLevelCommandManager>();
-        var allApps = AllAppsCommandProvider.Page;
+        var tlcManager = _serviceProvider.GetService<TopLevelCommandManager>()!;
+        tlcManager.PropertyChanged += TlcManager_PropertyChanged;
 
         // reference the TLC collection directly... maybe? TODO is this a good idea ot a terrible one?
-        _commands = tlcManager!.TopLevelCommands;
+        _commands = tlcManager.TopLevelCommands;
         _commands.CollectionChanged += Commands_CollectionChanged;
+
+        // This _should_ start a background thread to start loading all the apps.
+        // It _feels_ like it does it's work on the main thread though, like it
+        // slows down startup... which doesn't make sense.
+        _ = Task.Run(() =>
+        {
+            _ = AppCache.Instance.Value;
+            _appsLoading = false;
+            Debug.WriteLine("Loaded apps");
+            IsLoading = ActuallyLoading();
+        });
+    }
+
+    private void TlcManager_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(IsLoading))
+        {
+            IsLoading = ActuallyLoading();
+        }
     }
 
     private void Commands_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e) => RaiseItemsChanged(_commands.Count);
 
     public override IListItem[] GetItems()
     {
+        Debug.WriteLine("main GetItems");
         return string.IsNullOrEmpty(SearchText)
             ? _commands.Select(tlc => tlc).ToArray()
             : _filteredItems?.ToArray() ?? [];
@@ -53,5 +77,11 @@ public partial class MainListPage : DynamicListPage
         var allItems = commands.Concat(apps);
         _filteredItems = ListHelpers.FilterList(allItems, SearchText);
         RaiseItemsChanged(_filteredItems.Count());
+    }
+
+    private bool ActuallyLoading()
+    {
+        var tlcManager = _serviceProvider.GetService<TopLevelCommandManager>()!;
+        return _appsLoading || tlcManager.IsLoading;
     }
 }
