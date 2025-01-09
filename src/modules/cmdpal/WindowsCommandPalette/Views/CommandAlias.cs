d7da0392b6ffc91b2239cb3e004798a9b9faaecd
Stefan Markovic <stefan@janeasystems.com>
2025-01-09 22:43:27 +0100

    remove reference

--- src/modules/cmdpal/WindowsCommandPalette/Views/CommandAlias.cs
+++ src/modules/cmdpal/WindowsCommandPalette/Views/CommandAlias.cs
@@ -7,7 +7,6 @@ using Microsoft.CmdPal.Ext.Apps.Programs;
 using Microsoft.CmdPal.Ext.Bookmarks;
 using Microsoft.CmdPal.Ext.Calc;
 using Microsoft.CmdPal.Ext.Registry;
-using Microsoft.CmdPal.Ext.Settings;
 using Microsoft.CmdPal.Ext.WindowsServices;
 using Microsoft.CmdPal.Ext.WindowsSettings;
 using Microsoft.CmdPal.Ext.WindowsTerminal;
