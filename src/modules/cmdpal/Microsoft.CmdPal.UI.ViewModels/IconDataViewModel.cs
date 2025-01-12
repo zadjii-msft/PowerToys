c00525159af6d018d254cd7368331335e0cc3016
Mike Griese <zadjii@gmail.com>
2025-01-12 12:53:38 -0600

    the basics work

---
+++ src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/IconDataViewModel.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CmdPal.Extensions;
+using Microsoft.CmdPal.Extensions.Helpers;
+using Microsoft.CmdPal.UI.ViewModels.Models;
+using Windows.Storage.Streams;
+
+namespace Microsoft.CmdPal.UI.ViewModels;
+
+public partial class IconDataViewModel : ExtensionObjectViewModel
+{
+    private readonly ExtensionObject<IconData> _model = new(null);
+
+    // Locally cached properties from IconData.
+    public string Icon { get; private set; } = string.Empty;
+
+    // Streams are not trivially copyable, so we can't copy the data locally
+    // first. Hence why we're sticking this into an ExtensionObject
+    public ExtensionObject<IRandomAccessStreamReference> Data { get; private set; } = new(null);
+
+    public IconDataViewModel(IconData? icon, IPageContext errorContext)
+        : base(errorContext)
+    {
+        _model = new(icon);
+    }
+
+    // Unsafe, needs to be called on BG thread
+    public override void InitializeProperties()
+    {
+        var model = _model.Unsafe;
+        if (model == null)
+        {
+            return;
+        }
+
+        Icon = model.Icon;
+        Data = new(model.Data);
+    }
+}
---
+++ src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/IconInfoViewModel.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CmdPal.Extensions;
+using Microsoft.CmdPal.Extensions.Helpers;
+using Microsoft.CmdPal.UI.ViewModels.Models;
+using Windows.Storage.Streams;
+
+namespace Microsoft.CmdPal.UI.ViewModels;
+
+public partial class IconInfoViewModel : ExtensionObjectViewModel
+{
+    private readonly ExtensionObject<IconInfo> _model = new(null);
+
+    // These are properties that are "observable" from the extension object
+    // itself, in the sense that they get raised by PropChanged events from the
+    // extension. However, we don't want to actually make them
+    // [ObservableProperty]s, because PropChanged comes in off the UI thread,
+    // and ObservableProperty is not smart enough to raise the PropertyChanged
+    // on the UI thread.
+    public IconDataViewModel Light { get; private set; }
+
+    public IconDataViewModel Dark { get; private set; }
+
+    public IconInfoViewModel(IconInfo? icon, IPageContext errorContext)
+        : base(errorContext)
+    {
+        _model = new(icon);
+        Light = new(null, errorContext);
+        Dark = new(null, errorContext);
+    }
+
+    // Unsafe, needs to be called on BG thread
+    public override void InitializeProperties()
+    {
+        var model = _model.Unsafe;
+        if (model == null)
+        {
+            return;
+        }
+
+        Light = new(model.Light, this.PageContext);
+        Light.InitializeProperties();
+
+        Dark = new(model.Dark, this.PageContext);
+        Dark.InitializeProperties();
+    }
+}
