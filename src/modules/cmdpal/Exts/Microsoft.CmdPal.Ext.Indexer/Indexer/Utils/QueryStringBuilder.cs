3234e677ea1080bd5efb27361ef530d7145e64dd
Seraphima <zykovas91@gmail.com>
2025-01-13 13:32:15 +0100

    fuzzy match support

--- src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.Indexer/Indexer/Utils/QueryStringBuilder.cs
+++ src/modules/cmdpal/Exts/Microsoft.CmdPal.Ext.Indexer/Indexer/Utils/QueryStringBuilder.cs
@@ -2,8 +2,11 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Globalization;
+using System.Linq;
 using System.Text;
+using ManagedCommon;
 
 namespace Microsoft.CmdPal.Ext.Indexer.Indexer.Utils;
 
@@ -24,11 +27,33 @@ internal sealed class QueryStringBuilder
         var queryStr = new StringBuilder(SelectQueryWithScope);
 
         // Filter by item name display only
-        if (!string.IsNullOrEmpty(searchText))
+        if (!string.IsNullOrWhiteSpace(searchText))
         {
-            queryStr.Append(" AND (CONTAINS(System.ItemNameDisplay, '\"")
-                .Append(searchText)
-                .Append("*\"'))");
+            var appended = false;
+
+            try
+            {
+                var parts = searchText
+                    .Split([' '], StringSplitOptions.RemoveEmptyEntries)
+                    .Select(p => $"\"{p}*\"")
+                    .ToArray();
+
+                if (parts.Length > 0)
+                {
+                    var joinedParts = string.Join(" AND ", parts);
+                    queryStr.Append(" AND (CONTAINS(System.ItemNameDisplay, '").Append(joinedParts).Append("'))");
+                    appended = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("Failed to split query string", ex);
+            }
+
+            if (!appended)
+            {
+                queryStr.Append(" AND (CONTAINS(System.ItemNameDisplay, '\"").Append(searchText).Append("*\"'))");
+            }
         }
 
         // Always add reuse where to the query
