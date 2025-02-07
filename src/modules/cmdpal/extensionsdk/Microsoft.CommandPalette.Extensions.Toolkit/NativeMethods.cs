9c8fd5322aa48b0698d8c77f1ce422778f3cf296
Seraphima <zykovas91@gmail.com>
2025-02-07 16:55:15 +0100

    clean up

--- src/modules/cmdpal/extensionsdk/Microsoft.CommandPalette.Extensions.Toolkit/NativeMethods.cs
+++ src/modules/cmdpal/extensionsdk/Microsoft.CommandPalette.Extensions.Toolkit/NativeMethods.cs
@@ -8,50 +8,20 @@ namespace Microsoft.CommandPalette.Extensions.Toolkit;
 
 internal sealed class NativeMethods
 {
-    [DllImport("shell32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    internal static extern int SHCreateItemFromParsingName(
-            [MarshalAs(UnmanagedType.LPWStr)] string path,
-            nint pbc,
-            ref Guid riid,
-            [MarshalAs(UnmanagedType.Interface)] out IShellItem shellItem);
+    [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+    internal static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbFileInfo, uint uFlags);
 
-    [DllImport("gdi32.dll")]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    internal static extern bool DeleteObject(nint hObject);
-}
-
-[ComImport]
-[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-[Guid("43826d1e-e718-42ee-bc55-a1e261c37bfe")]
-public interface IShellItem
-{
-    void BindToHandler(
-        nint pbc,
-        [MarshalAs(UnmanagedType.LPStruct)] Guid bhid,
-        [MarshalAs(UnmanagedType.LPStruct)] Guid riid,
-        out nint ppv);
-
-    void GetParent(out IShellItem ppsi);
-
-    void GetDisplayName(SIGDN sigdnName, [MarshalAs(UnmanagedType.LPWStr)] out string ppszName);
-
-    void GetAttributes(uint sfgaoMask, out uint psfgaoAttribs);
-
-    void Compare(IShellItem psi, uint hint, out int piOrder);
-}
-
-/// <summary>
-/// The following are ShellItem DisplayName types.
-/// </summary>
-[Flags]
-public enum SIGDN : uint
-{
-    NORMALDISPLAY = 0,
-    PARENTRELATIVEPARSING = 0x80018001,
-    PARENTRELATIVEFORADDRESSBAR = 0x8001c001,
-    DESKTOPABSOLUTEPARSING = 0x80028000,
-    PARENTRELATIVEEDITING = 0x80031001,
-    DESKTOPABSOLUTEEDITING = 0x8004c000,
-    FILESYSPATH = 0x80058000,
-    URL = 0x80068000,
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SHFILEINFO
+    {
+#pragma warning disable SA1307 // Accessible fields should begin with upper-case letter
+        public IntPtr hIcon;
+        public int iIcon;
+        public uint dwAttributes;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+        public string szDisplayName;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+        public string szTypeName;
+#pragma warning restore SA1307 // Accessible fields should begin with upper-case letter
+    }
 }
--- src/modules/cmdpal/extensionsdk/Microsoft.CommandPalette.Extensions.Toolkit/ThumbnailHelper.cs
+++ src/modules/cmdpal/extensionsdk/Microsoft.CommandPalette.Extensions.Toolkit/ThumbnailHelper.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation
+// Copyright (c) Microsoft Corporation
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -13,11 +13,8 @@ namespace Microsoft.CommandPalette.Extensions.Toolkit;
 
 public class ThumbnailHelper
 {
-    public static readonly string ProgramDirectory = Directory.GetParent(path: AppContext.BaseDirectory)?.ToString() ?? string.Empty;
-    public static readonly int ThumbnailSize = 64;
-
     private static readonly string[] ImageExtensions =
-    {
+    [
         ".png",
         ".jpg",
         ".jpeg",
@@ -25,7 +22,7 @@ public class ThumbnailHelper
         ".bmp",
         ".tiff",
         ".ico",
-    };
+    ];
 
     public static IRandomAccessStream? GetThumbnail(string path)
     {
@@ -44,30 +41,13 @@ public class ThumbnailHelper
         return GetFileIconStream(path);
     }
 
-    [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
-    private static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbFileInfo, uint uFlags);
-
-    [StructLayout(LayoutKind.Sequential)]
-    private struct SHFILEINFO
-    {
-#pragma warning disable SA1307 // Accessible fields should begin with upper-case letter
-        public IntPtr hIcon;
-        public int iIcon;
-        public uint dwAttributes;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
-        public string szDisplayName;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
-        public string szTypeName;
-#pragma warning restore SA1307 // Accessible fields should begin with upper-case letter
-    }
-
     private const uint SHGFIICON = 0x000000100;
     private const uint SHGFILARGEICON = 0x000000000;
 
-    public static IRandomAccessStream GetFileIconStream(string filePath)
+    private static IRandomAccessStream GetFileIconStream(string filePath)
     {
-        var shinfo = default(SHFILEINFO);
-        SHGetFileInfo(filePath, 0, ref shinfo, (uint)Marshal.SizeOf(shinfo), SHGFIICON | SHGFILARGEICON);
+        var shinfo = default(NativeMethods.SHFILEINFO);
+        NativeMethods.SHGetFileInfo(filePath, 0, ref shinfo, (uint)Marshal.SizeOf(shinfo), SHGFIICON | SHGFILARGEICON);
 
         using var icon = Icon.FromHandle(shinfo.hIcon);
         var stream = new InMemoryRandomAccessStream();
