e08887a4fdd9c0c408c836c58b475adfd6fa9fb2
Mike Griese <migrie@microsoft.com>
2025-02-12 06:04:04 -0600

    Start making a dedicated extension settings page

--- src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/ProviderSettingsViewModel.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI.ViewModels/ProviderSettingsViewModel.cs
@@ -6,11 +6,13 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Microsoft.CmdPal.UI.ViewModels;
 
-public partial class ProviderSettingsViewModel(CommandProviderWrapper _provider, ProviderSettings _providerSettings) : ObservableObject
+public partial class ProviderSettingsViewModel(CommandProviderWrapper _provider, ProviderSettings _providerSettings, TopLevelCommandManager _tlcManager) : ObservableObject
 {
     public string DisplayName => _provider.DisplayName;
 
     public string ExtensionName => _provider.Extension?.ExtensionDisplayName ?? "Built-in";
+
+    public string ExtensionSubtext => $"{ExtensionName}, {TopLevelCommands.Count} commands";
 
     public IconInfoViewModel Icon => _provider.Icon;
 
@@ -18,5 +20,26 @@ public partial class ProviderSettingsViewModel(CommandProviderWrapper _provider,
     {
         get => _providerSettings.IsEnabled;
         set => _providerSettings.IsEnabled = value;
+    }
+
+    public List<TopLevelCommandItemWrapper> TopLevelCommands
+    {
+        get
+        {
+            var topLevelCommands = _tlcManager.TopLevelCommands;
+            var thisProvider = _provider;
+            var providersCommands = thisProvider.TopLevelItems;
+            List<TopLevelCommandItemWrapper> results = [];
+            foreach (var command in providersCommands)
+            {
+                var match = topLevelCommands.Where(tlc => tlc.Model.Unsafe == command).FirstOrDefault();
+                if (match != null)
+                {
+                    results.Add(match);
+                }
+            }
+
+            return results;
+        }
     }
 }
--- src/modules/cmdpal/Microsoft.CmdPal.UI/SettingsWindow.xaml.cs
+++ src/modules/cmdpal/Microsoft.CmdPal.UI/SettingsWindow.xaml.cs
@@ -2,28 +2,18 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.CmdPal.UI.Pages;
 using Microsoft.CmdPal.UI.ViewModels;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Graphics;
 
 namespace Microsoft.CmdPal.UI;
 
-public sealed partial class SettingsWindow : Window
+public sealed partial class SettingsWindow : Window,
+    IRecipient<NavigateToExtensionSettingsMessage>
 {
     public SettingsWindow()
     {
@@ -32,6 +22,8 @@ public sealed partial class SettingsWindow : Window
         this.AppWindow.SetIcon("ms-appx:///Assets/Icons/StoreLogo.png");
         this.AppWindow.Title = "Command Palette Settings";
         this.AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
+        PositionCentered();
+        WeakReferenceMessenger.Default.Register<NavigateToExtensionSettingsMessage>(this);
     }
 
     private void NavView_Loaded(object sender, RoutedEventArgs e)
@@ -63,4 +55,21 @@ public sealed partial class SettingsWindow : Window
             NavFrame.Navigate(pageType);
         }
     }
+
+    private void Navigate(ProviderSettingsViewModel extension) => NavFrame.Navigate(typeof(ExtensionPage), extension);
+
+    private void PositionCentered()
+    {
+        AppWindow.Resize(new SizeInt32 { Width = 1280, Height = 720 });
+        var displayArea = DisplayArea.GetFromWindowId(AppWindow.Id, DisplayAreaFallback.Nearest);
+        if (displayArea is not null)
+        {
+            var centeredPosition = AppWindow.Position;
+            centeredPosition.X = (displayArea.WorkArea.Width - AppWindow.Size.Width) / 2;
+            centeredPosition.Y = (displayArea.WorkArea.Height - AppWindow.Size.Height) / 2;
+            AppWindow.Move(centeredPosition);
+        }
+    }
+
+    public void Receive(NavigateToExtensionSettingsMessage message) => Navigate(message.ProviderSettingsVM);
 }
