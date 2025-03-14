53d2422834923ee5531da60f81e5a8dd5e1b7f63
Mike Griese <zadjii@gmail.com>
2025-03-14 16:37:46 -0500

    Revert "more feigned attempts at fixing the icons thing"
    This reverts commit b09418cf4bccba6d79cb1fcd9bc780eb5e6a22d5.

--- src/modules/cmdpal/extensionsdk/Microsoft.CommandPalette.Extensions.Toolkit/ThumbnailHelper.cs
+++ src/modules/cmdpal/extensionsdk/Microsoft.CommandPalette.Extensions.Toolkit/ThumbnailHelper.cs
@@ -2,7 +2,6 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
 using System.Runtime.InteropServices;
@@ -36,24 +35,7 @@ public class ThumbnailHelper
             }
             else
             {
-                // return GetFileIconStream(path);
-                return Task.Run(() =>
-                {
-                    IRandomAccessStream? result = null;
-                    var thread = new Thread(() =>
-                    {
-                        var t = GetFileIconStream(path);
-
-                        // t.ConfigureAwait(false);
-                        // t.Wait();
-                        // result = t.IsCompletedSuccessfully ? t.Result : null;
-                        result = t;
-                    });
-                    thread.SetApartmentState(ApartmentState.STA);
-                    thread.Start();
-                    thread.Join();
-                    return result;
-                });
+                return GetFileIconStream(path);
             }
         }
         catch (Exception)
@@ -65,55 +47,34 @@ public class ThumbnailHelper
 
     private const uint SHGFIICON = 0x000000100;
     private const uint SHGFILARGEICON = 0x000000000;
-    private const uint SHGFIUSEFILEATTRIBUTES = 0x000000010;
-    private const uint FILEATTRIBUTENORMAL = 0x00000080;
 
-    private static IRandomAccessStream? GetFileIconStream(string filePath)
+    private static async Task<IRandomAccessStream?> GetFileIconStream(string filePath)
     {
         var shinfo = default(NativeMethods.SHFILEINFO);
-        var hr = NativeMethods.SHGetFileInfo(filePath, FILEATTRIBUTENORMAL, ref shinfo, (uint)Marshal.SizeOf(shinfo), SHGFIICON | SHGFILARGEICON | SHGFIUSEFILEATTRIBUTES);
+        var hr = NativeMethods.SHGetFileInfo(filePath, 0, ref shinfo, (uint)Marshal.SizeOf(shinfo), SHGFIICON | SHGFILARGEICON);
 
         if (hr == 0 || shinfo.hIcon == 0)
         {
             return null;
         }
 
-        try
+        using var icon = Icon.FromHandle(shinfo.hIcon);
+        var stream = new InMemoryRandomAccessStream();
+        using (var memoryStream = new MemoryStream())
         {
-            // using var icon = Icon.FromHandle(shinfo.hIcon).Clone();
-            using var icon = (Icon)Icon.FromHandle(shinfo.hIcon).Clone();
-            using var bitmap = icon.ToBitmap();
-            var stream = new InMemoryRandomAccessStream();
-            bitmap.Save(stream.AsStream(), System.Drawing.Imaging.ImageFormat.Png);
-            stream.Seek(0);
+            icon.ToBitmap().Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+            memoryStream.Position = 0;
 
-            // using (var memoryStream = new MemoryStream())
-            // {
-            //    icon.ToBitmap().Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
-            //    memoryStream.Position = 0;
+            using var outputStream = stream.GetOutputStreamAt(0);
+            using var dataWriter = new DataWriter(outputStream);
+            dataWriter.WriteBytes(memoryStream.ToArray());
 
-            // using var outputStream = stream.GetOutputStreamAt(0);
-            //    using var dataWriter = new DataWriter(outputStream);
-            //    dataWriter.WriteBytes(memoryStream.ToArray());
-
-            // // dataWriter.StoreAsync().GetAwaiter().GetResult();
-            //    await dataWriter.StoreAsync();
-            //    await dataWriter.FlushAsync();
-            // }
-            return stream;
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine($"Failed to load icon for {filePath}");
-            Debug.WriteLine(ex.ToString());
-        }
-        finally
-        {
-            // Delete HIcon to avoid memory leaks
-            _ = NativeMethods.DestroyIcon(shinfo.hIcon);
+            // dataWriter.StoreAsync().GetAwaiter().GetResult();
+            await dataWriter.StoreAsync();
+            await dataWriter.FlushAsync();
         }
 
-        return null;
+        return stream;
     }
 
     private static async Task<IRandomAccessStream?> GetImageThumbnailAsync(string filePath)
